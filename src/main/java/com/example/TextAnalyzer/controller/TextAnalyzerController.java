74c7d77e0c26c5e1c36d12a94a56d52d02e93ff3
Mira <mira.schaar@gmx.net>
2023-11-12 20:26:42 +0100

    Added the WebConfiguration

--- src/main/java/com/example/TextAnalyzer/controller/TextAnalyzerController.java
+++ src/main/java/com/example/TextAnalyzer/controller/TextAnalyzerController.java
@@ -4,17 +4,15 @@ import com.example.TextAnalyzer.model.TextAnalysisRequest;
 import com.example.TextAnalyzer.model.TextAnalysisResponse;
 import com.example.TextAnalyzer.service.TextAnalyzerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class TextAnalyzerController {
     private final TextAnalyzerService textAnalyzerService;
@@ -54,5 +52,10 @@ public class TextAnalyzerController {
 
     }
 
-
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
+    }
 }
